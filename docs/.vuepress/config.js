a28056803072b00d30b02ffdf6b409e28a1997b8
fudongwei <fudongwei@yongxingsec.com>
2022-11-02 14:50:59 +0800

    feat：配置文件更新

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,20 @@
 import { defineUserConfig } from 'vuepress'
+import { defaultTheme } from 'vuepress'
 
-export default defineUserConfig({
+export default ({
     lang: 'zh-CN',
     title: 'Emily Blog',
-    description: 'Emily的博客'
+    description: 'Emily的博客',
+    theme: defaultTheme({
+        navbar: [
+            {
+                text: '首页',
+                link: '/'
+            },
+            {
+                text: '工具',
+                link: '/工具/maven.html'
+            }
+        ]
+    })
 })
